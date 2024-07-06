6a3499572622a1e427405ddbcc57ff7d5413f095
NotMeMike <leminhquan1532@gmail.com>
2024-07-06 11:02:19 +0700

    Monke

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+  DB_SERVER: 'cluster.jrkicpx.mongodb.net',
+  DB_USER: 'leeminwuan',
+  DB_PASS: '123',
+  DB_DATABASE: 'shoppingonline',
+  JWT_SECRET: '1234',
+  JWT_EXPIRES: '9999999999999999', // in milliseconds
+  EMAIL_USER: 'popih22@gmail.com', // gmail service
+  EMAIL_PASS: 'owco ofcr worl ocvf'
+};
+module.exports = MyConstants;
